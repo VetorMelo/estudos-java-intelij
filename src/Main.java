8b110530eed658d1c59d02a6b05dcba64e44a91e
Victor Melo <contatomelo2@gmail.com>
2025-03-30 00:53:28 -0300

    estudadno padroes de desenvolvimento e conceitos

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /* System.out.println("Hello World!");
+        System.out.println("argumento 1:" + args[0]);
+        System.out.println("argumento 2:" + args[1]); */
+
+        //estudando padroes de desenvolvimento e conceitos
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Olá, informe seu nome");
+        String name = scanner.next();
+        System.out.println("Informe sua idade");
+        int age = scanner.nextInt();
+        System.out.printf("Olá %s sua idade é %s \n", name, age);
     }
 }
